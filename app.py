63f9a174c75ceb4809e925619e18d596fa88ca4a
Ashwath <ashwathguru.s@bnedloudcloud.com>
2023-11-29 23:11:19 +0530

    adding talk back feature

--- app.py
+++ app.py
@@ -1,12 +1,28 @@
 import streamlit as st
 from bokeh.models.widgets import Button
 from bokeh.models import CustomJS
+import openai
+from langchain.agents import create_csv_agent
+from langchain.llms import OpenAI
 
-text = st.text_input("Say what ?")
+openai.api_key = st.secrets["OPENAI_API_KEY"]
 
-tts_button = Button(label="Speak", width=100)
+def get_answer_csv(query: str) -> str:
+    file = "raw.csv"
+    agent = create_csv_agent(OpenAI(temperature=0), file, verbose=False)
+    answer = agent.run(query)
+    return answer
 
-tts_button.js_on_event("button_click", CustomJS(code=f"""
+st.header("Chat with TicketGPT and it talks back!")
+
+query = st.text_area("Ask any question related to the tickets")
+button = st.button("Submit")
+if button:
+  st.write(get_answer_csv(query))
+  text = get_answer_csv(query)
+  tts_button = Button(label="Play", width=100)
+
+  tts_button.js_on_event("button_click", CustomJS(code=f"""
     var u = new SpeechSynthesisUtterance();
     u.text = "{text}";
     u.lang = 'en-US';
@@ -14,4 +30,4 @@ tts_button.js_on_event("button_click", CustomJS(code=f"""
     speechSynthesis.speak(u);
     """))
 
-st.bokeh_chart(tts_button)
+  st.bokeh_chart(tts_button)
